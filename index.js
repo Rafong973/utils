09b5b471eb65a8f14321f3460e43cefc55f1c16b
Rafong <rafong21@outlook.com>
2023-05-23 16:14:11 +0800

    feat:新增方法；修改打包方式

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./lib/bundle.min.js')
+} else {
+  module.exports = require('./lib/bundle.js')
+}
--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,16 @@ const terser = require('@rollup/plugin-terser')
 
 module.exports = {
   input: 'src/index.js',
-  output: {
-    file: 'lib/bundle.js',
-    format: 'cjs'
-  },
-  plugins: [terser()]
+  output: [
+    {
+      file: 'lib/bundle.js',
+      format: 'es'
+    },
+    {
+      file: 'lib/bundle.min.js',
+      format: 'iife',
+      name: 'version',
+      plugins: [terser()]
+    }
+  ]
 }
